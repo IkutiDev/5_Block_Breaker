b8dc4b315842012be6f9e93613a363d62038ee52
Ikuti <m.j.olkiewicz@gmail.com>
2019-07-11 19:47:17 +0200

    Paddle Movement added

---
+++ Brick Smasher/Assets/Scripts/Paddle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] private float screenWidthInUnits = 16f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseXPosition = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        //Debug.Log(mouseXPosition);
+        Vector2 paddlesPos = new Vector2(mouseXPosition, transform.position.y);
+        transform.position = paddlesPos;
+    }
+}
