6bae49ffd11c578f2fdeeb99eefef05ed2bbb0c9
Ikuti <m.j.olkiewicz@gmail.com>
2019-07-11 22:33:56 +0200

    Added ball following paddle and a working paddle launching.

---
+++ Brick Smasher/Assets/Scripts/Ball.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    //config
+    [SerializeField] private Paddle paddle;
+
+    [SerializeField] private Vector2 ballVelocity = new Vector2(2f,15f);
+    //state
+    private Vector2 _paddleToBallVector2;
+
+    private bool _hasStarted;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _paddleToBallVector2 = transform.position - paddle.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = ballVelocity;
+            _hasStarted = true;
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePosition = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+        transform.position = _paddleToBallVector2 + paddlePosition;
+    }
+}
--- Brick Smasher/Assets/Scripts/Paddle.cs
+++ Brick Smasher/Assets/Scripts/Paddle.cs
@@ -6,6 +6,8 @@ using UnityEngine.Serialization;
 public class Paddle : MonoBehaviour
 {
     [SerializeField] private float screenWidthInUnits = 16f;
+    [SerializeField] private float minX = 1f;
+    [SerializeField] private float maxX = 15f;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +19,8 @@ public class Paddle : MonoBehaviour
     {
         float mouseXPosition = Input.mousePosition.x / Screen.width * screenWidthInUnits;
         //Debug.Log(mouseXPosition);
-        Vector2 paddlesPos = new Vector2(mouseXPosition, transform.position.y);
+        Vector2 paddlesPos = new Vector2(transform.position.x, transform.position.y);
+        paddlesPos.x = Mathf.Clamp(mouseXPosition,minX,maxX);
         transform.position = paddlesPos;
     }
 }
