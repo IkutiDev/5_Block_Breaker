d76b7a73b4e5b76197e7c5824f743c3d60549504
Ikuti <m.j.olkiewicz@gmail.com>
2019-07-23 20:24:40 +0200

    Added break block sound effect

--- Brick Smasher/Assets/Scripts/Block.cs
+++ Brick Smasher/Assets/Scripts/Block.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] private AudioClip breakingSoundEffect;
+    [SerializeField] private float breakingSoundEffectVolume=0.8f;
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (Camera.main != null) AudioSource.PlayClipAtPoint(breakingSoundEffect, Camera.main.transform.position, breakingSoundEffectVolume);
         Destroy(gameObject);
-        //Debug.Log(collision.gameObject.name);
     }
 }
