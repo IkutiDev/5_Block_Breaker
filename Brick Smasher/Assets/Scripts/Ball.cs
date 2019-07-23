5319895f9361f1482a96e63cfc988446ecf41f14
Ikuti <m.j.olkiewicz@gmail.com>
2019-07-23 20:13:17 +0200

    Changed up the levels, added ball sound effect(multiple), fixed up 4x4 block prefab

--- Brick Smasher/Assets/Scripts/Ball.cs
+++ Brick Smasher/Assets/Scripts/Ball.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
 public class Ball : MonoBehaviour
 {
@@ -9,17 +10,21 @@ public class Ball : MonoBehaviour
     [SerializeField] private Paddle paddle;
 
     [SerializeField] private Vector2 ballVelocity = new Vector2(2f,15f);
+
+    [SerializeField] private AudioClip[] ballSoundEffects;
     //state
     private Vector2 _paddleToBallVector2;
 
     private bool _hasStarted;
-
+    private AudioSource _audioSource;
     private void Awake()
     {
         if (paddle == null)
         {
             paddle=FindObjectOfType<Paddle>();
         }
+
+        _audioSource = GetComponent<AudioSource>();
     }
 
     // Start is called before the first frame update
@@ -51,5 +56,14 @@ public class Ball : MonoBehaviour
     {
         Vector2 paddlePosition = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
         transform.position = _paddleToBallVector2 + paddlePosition;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (_hasStarted)
+        {
+            AudioClip clip = ballSoundEffects[UnityEngine.Random.Range(0, ballSoundEffects.Length)];
+            _audioSource.PlayOneShot(clip);
+        }
     }
 }
--- Brick Smasher/Assets/Scripts/Paddle.cs
+++ Brick Smasher/Assets/Scripts/Paddle.cs
@@ -8,17 +8,11 @@ public class Paddle : MonoBehaviour
     [SerializeField] private float screenWidthInUnits = 16f;
     [SerializeField] private float minX = 1f;
     [SerializeField] private float maxX = 15f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         float mouseXPosition = Input.mousePosition.x / Screen.width * screenWidthInUnits;
-        //Debug.Log(mouseXPosition);
         Vector2 paddlesPos = new Vector2(transform.position.x, transform.position.y);
         paddlesPos.x = Mathf.Clamp(mouseXPosition,minX,maxX);
         transform.position = paddlesPos;
