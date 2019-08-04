5cd3c91cdd417cfa4de9d41ba959badf23a64bcc
Ikuti <m.j.olkiewicz@gmail.com>
2019-08-04 11:50:55 +0200

    Block counting works

--- Brick Smasher/Assets/Scripts/Block.cs
+++ Brick Smasher/Assets/Scripts/Block.cs
@@ -7,9 +7,21 @@ public class Block : MonoBehaviour
 {
     [SerializeField] private AudioClip breakingSoundEffect;
     [SerializeField] private float breakingSoundEffectVolume=0.8f;
+    private Level _level;
+
+    private void Start()
+    {
+        _level = FindObjectOfType<Level>();
+        _level.CountBreakableBlocks();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (Camera.main != null) AudioSource.PlayClipAtPoint(breakingSoundEffect, Camera.main.transform.position, breakingSoundEffectVolume);
         Destroy(gameObject);
+    }
+    void OnDestroy()
+    {
+        Debug.Log("OnDestroy1");
     }
 }
---
+++ Brick Smasher/Assets/Scripts/Level.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    private int _breakableBlocks;
+
+    public void CountBreakableBlocks()
+    {
+        _breakableBlocks++;
+    }
+}
